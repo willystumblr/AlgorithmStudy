6206e9926e09e5d1e9387f555445c49087b14138
willystumblr <71213700+willystumblr@users.noreply.github.com>
2021-05-16 01:16:50 +0900

    Add files via upload

---
+++ ch#1,2/1475.py
@@ -0,0 +1,14 @@
+room = input()
+sixplusnine = 0
+counts = []
+for i in range(10):
+    counts.append(room.count(str(i)))
+    if i==6 or i==9:
+        counts[i] = 0
+        sixplusnine += room.count(str(i))
+
+N = max(counts)
+M = sixplusnine//2 if sixplusnine%2==0 else sixplusnine//2 +1
+
+print(max(N, M))
+    
