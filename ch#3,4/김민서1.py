b6e4c6176ea9a02e747cd2d2b090c94106894e50
willystumblr <71213700+willystumblr@users.noreply.github.com>
2021-05-20 04:04:35 +0900

    Create 김민서1.py

---
+++ ch#3,4/김민서1.py
@@ -0,0 +1,35 @@
+#백준 1260
+#704ms인데 1등이랑 10배 차이남..
+
+from collections import deque 
+
+def dfs(start):
+    visited[start] = True
+    print(start, end = ' ')
+
+    for k in range(1, n+1):
+        if not visited[k] and graph[start][k] == 1:
+            dfs(k)
+def bfs(start):
+    queue = deque([start])
+    visited_2[start] = True
+    while queue:
+        elem = queue.popleft()
+        print(elem, end = ' ')
+        for k in range(1, n+1):
+            if not visited_2[k] and graph[elem][k] == 1:
+                queue.append(k)
+                visited_2[k] = True
+
+n, m, start = map(int, input().split())
+graph = [[0]*(n+1) for _ in range(n+1)]
+visited = [False for _ in range(n+1)]
+visited_2 = [False for _ in range(n+1)]
+for i in range(m):
+    x, y = map(int, input().split())
+    graph[x][y] = 1
+    graph[y][x] = 1
+
+dfs(start)
+print()
+bfs(start)
